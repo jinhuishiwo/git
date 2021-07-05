6d9999fcf9b520f67df3309bf93aa8d17d70610d
jinhuishiwo <1151452264@qq.com>
2021-07-05 21:28:13 +0800

    初次上传2

--- src/main/java/com/bjpowernode/Main.java
+++ src/main/java/com/bjpowernode/Main.java
@@ -1,7 +1,15 @@
 package com.bjpowernode;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        Date d=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss SSS");
+        String time=sdf.format(d);
+        System.out.println(time);
     }
 }
